35ddb08447f367b2922e37a2d63517bd6ec60dcd
windofxy <1594251995@qq.com>
2025-02-14 13:30:02 +0800

    Update V1.0.6.1
    改善兼容性

--- VPet.Plugin.MultiChannelMusicFix/Properties/AssemblyInfo.cs
+++ VPet.Plugin.MultiChannelMusicFix/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Windofxy@Github")]
 [assembly: AssemblyProduct("VPet.Plugin.MultiChannelMusicFix")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright ©  2025 Windofxy@Github")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.6.0")]
-[assembly: AssemblyFileVersion("1.0.6.0")]
+[assembly: AssemblyVersion("1.0.6.1")]
+[assembly: AssemblyFileVersion("1.0.6.1")]
